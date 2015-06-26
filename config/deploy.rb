75a25bc1a6b0c9c7e4a3fa9118494aab36bb87fe
Hongchao Jiang <JHC@Hongchaos-MacBook-Air.local>
2015-06-26 19:14:25 +0800

    Edited files for deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,8 @@ lock '3.4.0'
 
 set :application, 'nushost'
 set :repo_url, 'https://github.com/jianghc93/nushost.git'
-set :rails_env, "production"
+set :branch, 'capistrano-test'
+#set :rails_env, "production"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
@@ -24,7 +25,7 @@ set :deploy_to, '/home/rails'
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -60,4 +60,4 @@
 #     # password: 'please use keys'
 #   }
 
-server '128.199.181.27', user: 'root', roles: %w{web app}
+server '128.199.181.27', user: 'root', roles: %w{web app db}
